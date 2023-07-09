e55d36b61402892be4ae9ccaed771718a1bd263e
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2023-07-09 13:37:14 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Header from './Components/Header'
-import Card from './Components/Card'
 import "./App.css";
-import { getAllTodo, addTodo, updateTodo, deleteTodo } from "./utils/HandleApi";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import AddBlog from "./Components/AddBlog";
 import HomeScreen from "./Components/HomeScreen";
 import ReadScreen from "./Components/ReadScreen";
 
 function App() {
-  const [todo, setTodo] = useState([]);
-  const [text, setText] = useState("");
-  const [image, setImage] = useState("");
-  const [description, setDescription] = useState("");
-  const [isUpdating, setIsUpdating] = useState(false);
-  const [todoId, setTodoId] = useState("");
-
-  useEffect(() => {
-    getAllTodo(setTodo);
-  }, []);
-
-  const updateMode = (_id, text, descript) => {
-    setIsUpdating(true);
-    setDescription(descript);
-    setText(text);
-    setTodoId(_id);
-  };
 
   return (
     <div className="background">
